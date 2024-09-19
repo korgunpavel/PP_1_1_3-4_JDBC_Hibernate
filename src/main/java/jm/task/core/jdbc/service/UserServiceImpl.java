ea98176587c75d988eacfdc62c2f372fd86dbcc1
Pavel Korgun <korgunpaul@gmail.com>
2024-09-19 16:07:53 +0500

    repairs

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -2,11 +2,10 @@ package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
 
 import java.util.List;
 
-public class UserServiceImpl extends Util implements UserService {
+public class UserServiceImpl implements UserService {
 
     private final UserDaoHibernateImpl userDaoHibernate;
 
